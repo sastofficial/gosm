b85339bcedd1d86d8977bc63cb67b4dec14d2474
sastofficial <39431922+sastofficial@users.noreply.github.com>
2022-02-08 17:34:29 +0100

    changed redirect speed to 0

--- GOSM/GOSM.py
+++ GOSM/GOSM.py
@@ -3,7 +3,7 @@ import os
 html1 = """<h1 style="text-align: center;">Look At This Image</h1>
 <h1 style="text-align: center;"><img src=""" # dont forget to add a extra " when making the html document.
 html2 = """/></h1>
-<meta http-equiv="refresh" content="1; URL='steam://rungameid/""" # dont forget to add another " at the begining of this.
+<meta http-equiv="refresh" content="0; URL='steam://rungameid/""" # dont forget to add another " at the begining of this.
 html3 = " />"
 
 input("Welcome to GOSM press enter to continue") # thx to https://stackoverflow.com/questions/19374944/how-to-wait-for-enter-key-press-in-python-2-7
